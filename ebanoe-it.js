e57e359031594ec0036630708eebee70a61d0837
tup1tsa <natorvano.work@gmail.com>
2018-12-15 10:42:31 +0200

    ebanoe-it new-comments

---
+++ ebanoe-it.js
@@ -0,0 +1,15 @@
+var key=document.location.href.replace(/^.*\/([^/]+)(\/#?.*)$/,'__sc-$1');
+
+var sc=localStorage.getItem(key)||'';
+
+sc.split(',').forEach(el=>jQuery('#comment-'+el).addClass('seen'));
+
+jQuery('.comment:not(.seen)>.comment-body').css({borderLeft: "5px solid #800"});
+
+sc=[];
+
+jQuery('.comment').each((i, el) => sc.push(el.id.substring(8)));
+
+localStorage.setItem(key, sc.join(','));
+
+console.log(jQuery('.comment:not(.seen)').length+' new comments');
